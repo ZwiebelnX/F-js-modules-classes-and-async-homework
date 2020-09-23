19de4be060fb602fddc065db18a24a7631736fda
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-23 17:30:15 +0800

    feedback

--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -2,5 +2,6 @@ import urlInfo from "./constant";
 
 const getURL = () => `${urlInfo.ADDRESS}:${urlInfo.PORT}${urlInfo.PATH}`;
 
+// TODO feedback：utils里面的内容尽量都用export导出
 export const region = urlInfo.REGION;
 export default getURL;
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,6 +1,9 @@
 export const getPoetry = async () => {
+  // TODO feedback：url可以抽成变量
   const response = await fetch("https://v1.jinrishici.com/all.json");
   const data = await response.json();
+
+  // TODO feedback: 可以用ES6解构
   const result = [];
   result.push(data.origin);
   result.push(data.author);
